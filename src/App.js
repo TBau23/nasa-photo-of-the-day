8f556a2b89ddcab3436b86e8ef38bd7ea3d95388
Tom Bauer <tbauer1898@gmail.com>
2020-08-12 17:10:28 -0500

    Done with MVP.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+
+import PrimaryDiv from './Components/PrimaryDiv'
+
 
 function App() {
+  
+  const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+  const API_KEY = 'OtfbpAIdh5pPnTEXte62zlEQjMNlThE2FbKitfNW'
+  const [nasaData, setNasaData] = useState({})
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        setNasaData(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+
+  }, [])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <PrimaryDiv 
+    nasaData={nasaData}
+    setNasaData={setNasaData}
+    />
+    
     </div>
   );
 }
